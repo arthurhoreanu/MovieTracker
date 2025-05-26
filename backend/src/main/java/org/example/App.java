c326bbeeec06f2067a336880516a8c744bd156e0
AlexiaMariaV <alexiavoina2004@gmail.com>
2025-05-26 19:53:01 +0300

    Added Movie entity, Added DBConnection for PostgreSQL, added MovieRepository with base operations like create, insert, delete, read and other helping methods. Added a small test in App

--- backend/src/main/java/org/example/App.java
+++ backend/src/main/java/org/example/App.java
@@ -1,9 +1,51 @@
 package org.example;
 
+import org.example.Model.Movie;
+import org.example.Repository.MovieRepository;
+
+
+import java.util.List;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MovieRepository repo = new MovieRepository();
+
+        // 1. Add Movie
+        Movie newMovie = new Movie(
+                "The Matrix",
+                1999,
+                "Sci-Fi",
+                "Netflix",
+                false,
+                "https://example.com/matrix.jpg"
+        );
+
+        repo.insert(newMovie);
+        System.out.println("✅ Movie added.");
+
+        // 2. Show all movies
+        List<Movie> allMovies = repo.findAll();
+        System.out.println("\n🎬 All movies:");
+        allMovies.forEach(System.out::println);
+
+        // 3. Mark as watched
+        repo.markWatched("The Matrix");
+        System.out.println("\n✅ The movie 'The Matrix' marked as seen.");
+
+        // 4. Show only watched movies
+        List<Movie> seen = repo.findWatched();
+        System.out.println("\n👀 Watched movies:");
+        seen.forEach(System.out::println);
+
+        // 5. Delete movie
+        repo.deleteByTitle("The Matrix");
+        System.out.println("\n🗑️ The movie 'The Matrix' was deleted.");
+
+        // 6. Final test
+        List<Movie> ramase = repo.findAll();
+        System.out.println("\n📦 Remaining movies in DB:");
+        ramase.forEach(System.out::println);
     }
 }
---
+++ backend/src/main/java/org/example/Repository/DBConnection.java
@@ -0,0 +1,20 @@
+package org.example.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/movie-tracker";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1233";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
